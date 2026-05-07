ad3970c4006f399765c161a219380eb75173216d
Joser404 <heyhema94@gmail.com>
2026-05-07 12:34:01 +0700

    Revert "add Suppplier and fix UI"

--- src/FE/src/App.jsx
+++ src/FE/src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import Showcase from './pages/Showcase'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
 import ApprovalWait from './pages/ApprovalWait'
+import Dashboard from './pages/Dashboard'
 import AdminDashboard from './pages/AdminDashboard'
 import StaffDashboard from './pages/StaffDashboard'
-import Suppliers from './pages/Suppliers'
 import ForgotPassword from './pages/ForgotPassword'
 import ForgotPasswordUUID from './pages/ForgotPasswordUUID'
 import VerifyUUID from './pages/VerifyUUID'
@@ -14,18 +15,18 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Showcase />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/approval" element={<ApprovalWait />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/staff" element={<StaffDashboard />} />
-        <Route path="/admin/suppliers" element={<Suppliers userRole="admin" />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-uuid" element={<ForgotPasswordUUID />} />
         <Route path="/verify-uuid" element={<VerifyUUID />} />
         <Route path="/new-password" element={<NewPassword />} />
-        <Route path="*" element={<Navigate to="/login" replace />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   )
